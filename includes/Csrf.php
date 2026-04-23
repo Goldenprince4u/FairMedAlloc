659a4abbe671de165c3a14637cf4f9dd4efd8e56
AbdulQuadri Olagunju <quadridaboss4u@gmail.com>
2026-04-24 00:41:36 +0100

    Cleanup: Remove empty Csrf.php stub, unused footer.php, pycache, and one-time scripts

--- includes/Csrf.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This file has been intentionally emptied.
-// CSRF protection is handled by procedural functions in includes/security_helper.php:
-//   generate_csrf_token(), check_csrf(), csrf_field(), verify_csrf_token()
-?>
